9852028868ed9bbf0d6c0664b7bb1378150fb4c8
ThatSealgair <sealgair@X1-Windows.localdomain>
2022-11-20 01:29:24 +1000

    Don't need to upload target folder

--- AOC/2022/rust-solutions/target/debug/deps/rust_solutions-a5bdc8a4be7d1f3c.d
+++
@@ -1,5 +0,0 @@
-/mnt/d/Documents/Programming/SPTDDTOR/AOC/2022/rust-solutions/target/debug/deps/rust_solutions-a5bdc8a4be7d1f3c.rmeta: src/main.rs
-
-/mnt/d/Documents/Programming/SPTDDTOR/AOC/2022/rust-solutions/target/debug/deps/rust_solutions-a5bdc8a4be7d1f3c.d: src/main.rs
-
-src/main.rs:
--- AOC/2022/rust-solutions/target/debug/deps/rust_solutions-e405508ed3b8c027.d
+++
@@ -1,5 +0,0 @@
-/mnt/d/Documents/Programming/SPTDDTOR/AOC/2022/rust-solutions/target/debug/deps/rust_solutions-e405508ed3b8c027.rmeta: src/main.rs
-
-/mnt/d/Documents/Programming/SPTDDTOR/AOC/2022/rust-solutions/target/debug/deps/rust_solutions-e405508ed3b8c027.d: src/main.rs
-
-src/main.rs:
